eb689b8a0ca9ee5172094bedda2531b8fd3af16d
Alex Simion <alexsam20032003@yahoo.com>
2018-07-30 21:04:58 +0300

    1st

---
+++ music/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+#admin/Pass1234
+from .models import Album, Song
+
+# Register your models here.
+admin.site.register(Album)
+admin.site.register(Song)
---
+++ music/urls.py
@@ -0,0 +1,36 @@
+"""website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+app_name = 'music'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    # path('^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+]
